13c230253ee75ad2045202bdd1c76335529e092a
plimsoftware <plimsoftware@zonmail.pt>
2020-07-01 13:48:44 +0100

    cors

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://storeapi.plimsoftware.pt',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
 });
